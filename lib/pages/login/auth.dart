7f77c8dfe52be7ffcfe239fca57b4f32936956d3
andrefreitasAM <a16631@alunos.ipca.pt>
2024-05-24 13:35:02 +0100

    Login/Register UI

---
+++ lib/pages/login/auth.dart
@@ -0,0 +1,15 @@
+import 'package:adrenture/models/user.dart';
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: ,
+        builder: (context, snapshot) ,),
+    );
+  }
+}
